6f69fb359c27a608063958ee45dacb69f3630ab9
Adie Chris <git config --global user.email>
2020-03-09 00:25:56 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,23 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCircle, faGripVertical, faHome} from '@fortawesome/free-solid-svg-icons';
-import { faInstagram, faYoutube, faLinkedin , faWhatsapp, faTelegram } from '@fortawesome/free-brands-svg-icons';
+import { faCircle, faGripVertical, faHome, faChalkboardTeacher, faChartLine, faTag, faCheck} from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faYoutube, faLinkedin , faWhatsapp, faTelegram, } from '@fortawesome/free-brands-svg-icons';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SubscriptionComponent } from './subscription/subscription.component';
+import { TestimonyComponent } from './testimony/testimony.component';
 
-library.add(faTelegram, faInstagram, faYoutube, faLinkedin, faWhatsapp, faCircle,  faGripVertical, faHome);
+// tslint:disable-next-line: max-line-length
+library.add(faTelegram, faInstagram, faYoutube, faLinkedin, faWhatsapp, faCircle,  faGripVertical, faHome, faChalkboardTeacher, faChartLine, faTag, faCheck);
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    SubscriptionComponent,
+    TestimonyComponent
   ],
   imports: [
     BrowserModule,
